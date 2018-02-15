4a023b206c132fea3bafc80c77a9716f113d9fe9
Jane Kim <Janeekim12@gmail.com>
2018-02-15 09:21:56 -0700

    Refactoring for flashcards, guess, and deck tests

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,17 +6,18 @@ require './lib/flashcards'
 require 'pry'
 
 class DeckTest < MiniTest::Test
-   def test_deck_test_exists
-     card_1 = Card.new("What is the capital of Alaska?", "Juneau")
-     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
-     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+   def setup
+     @card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
    end
 
-   def Test_deck_holds_cards
+   def test_deck_holds_cards
+     skip
      card_1 = Card.new("What is the capital of Alaska?", "Juneau")
      card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
      card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
-     
+
    end
 
 end
--- test/flashcard_test.rb
+++ test/flashcard_test.rb
@@ -5,22 +5,20 @@ require 'pry'
 
 class CardTest < MiniTest::Test
 
-  def test_card_exisits
-    card = Card.new("What is the capital of Alaska?", "Juneau")
+  def setup
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+  end
 
-    assert_instance_of Card, card
+  def test_card_exisits
+    assert_instance_of Card, @card_1
   end
 
   def test_it_has_a_question
-   card = Card.new("What is the capital of Alaska?", "Juneau")
-
-   assert_equal "What is the capital of Alaska?", card.question
+   assert_equal "What is the capital of Alaska?", @card_1.question
   end
 
   def test_the_question_has_an_answer
-    card = Card.new("What is the capital of Alaska?", "Juneau")
-
-    assert_equal "Juneau", card.answer
+    assert_equal "Juneau", @card_1.answer
   end
 
 end
