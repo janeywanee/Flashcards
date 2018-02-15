1ec6799d59b1c8ce2aed7b1d42ae6449ba70a2d5
Jane Kim <Janeekim12@gmail.com>
2018-02-15 11:06:27 -0700

    stuck on test_deck_holds_cards, undefined method cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -10,22 +10,21 @@ class DeckTest < MiniTest::Test
      @card_1 = Card.new("What is the capital of Alaska?", "Juneau")
      @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
      @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+     @deck = Deck.new([@card_1, @card_2, @card_3])
+   end
+
+   def test_deck_exists
+     assert_instance_of Deck, @deck
    end
 
    def test_deck_holds_cards
-     skip
-     card_1 = Card.new("What is the capital of Alaska?", "Juneau")
-     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
-     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
-
+     assert_equal [@card_1, @card_2, @card_3], Deck.cards
+     binding.pry
    end
 
 end
 
 
 
-# deck = Deck.new([card_1, card_2, card_3])
-# deck.cards
-# => [card_1, card_2, card_3]
 # deck.count
 # => 3
