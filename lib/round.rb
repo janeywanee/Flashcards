cb0f501f25af5de0941fbaa90e1c14d0b385b8f9
Jane Kim <Janeekim12@gmail.com>
2018-02-15 17:55:53 -0700

    started test_record_guess

--- lib/round.rb
+++ lib/round.rb
@@ -1,2 +1,13 @@
-# class Round
-# end
+class Round
+  attr_reader :deck, :guesses
+
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+  end
+
+  def current_card
+    @deck.cards[0]
+  end
+
+end
--- test/round_test.rb
+++ test/round_test.rb
@@ -1,13 +1,66 @@
-# require 'minitest/autorun'
-# require 'minitest/pride'
-# require './lib/round'
-# require './lib/deck'
-# require './lib/guess'
-# require './lib/flashcards'
-# require 'pry'
-#
-# class RoundTest < MiniTest::Test
-#    def setup
-#
-#    end
-#  end
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/round'
+require './lib/deck'
+require './lib/guess'
+require './lib/flashcards'
+require 'pry'
+
+class RoundTest < MiniTest::Test
+   def setup
+     @card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+     @card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+     @deck = Deck.new([@card_1, @card_2])
+     @round = Round.new(@deck)
+   end
+
+   def test_round_exists
+     assert_instance_of Round, @round
+   end
+
+   def test_round_has_a_deck
+     assert @deck, @round.deck
+   end
+
+   def test_round_has_guesses
+     assert_equal [], @round.guesses
+   end
+
+   def test_round_has_a_current_card
+     assert_equal @card_1 ,@round.current_card
+   end
+
+   def test_has_a_record_guess
+     assert_equal , @round.record_guess
+   end
+ end
+
+
+
+
+
+
+
+ # round.record_guess("Juneau")
+ # => #<Guess:0x007ffdf19c8a00 @card=#<Card:0x007ffdf1820a90 @answer="Juneau", @question="What is the capital of Alaska?">, @response="Juneau">
+
+
+
+ # round.guesses.count
+ # => 1
+ # round.guesses.first.feedback
+ # => "Correct!"
+ # round.number_correct
+ # => 1
+ # round.current_card
+ # => #<Card:0x007ffdf1820a90 @answer="93,000,000", @question="Approximately how many miles are in one astronomical unit?">
+ # round.record_guess("2")
+ # => #<Guess:0x007ffdf19c8a00 @card=#<Card:0x007ffdf1820a90 @answer="93,000,000", @question="Approximately how many miles are in one astronomical unit?">, @response="2">
+ # round.guesses.count
+ # => 2
+ # round.guesses.last.feedback
+ # => "Incorrect."
+ # round.number_correct
+ # => 1
+ # round.percent_correct
+ # => 50
