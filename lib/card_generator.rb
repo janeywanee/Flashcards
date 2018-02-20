b47e53e9687a742a65918d6fce91c07d8d4a1e1b
Jane Kim <Janeekim12@gmail.com>
2018-02-20 10:27:42 -0700

    made cahnges to card gen

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,22 +1,26 @@
 require "pry"
 class CardGenerator
+  attr_accessor :cards
 
-  def initialize(cards)
+  def initialize(file)
     @cards = []
-  end
-
-  def create_cards
-    @create_cards << Card.new(line)
-  end
-
-  File.readlines("card.txt").each do |line|
-    puts line
+    File.open("card.txt", "r").each do |line|
+      data = line.split(/\n/)
+      question, answer = data.map{ |line| line.split(",") }.flatten
+      card = Card.new(question, answer)
+      @cards.push(card)
+    end
   end
 end
 
+  # def create_cards
+  #   @create_card << Card.new(line)
+  # end
 
 
-# create a card generator class 
+
+
+# create a card generator class
 # have a method that returns an array of cards
 # in the method you will take the lines and create Cards
 # Card.new(line)
